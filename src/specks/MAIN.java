ab75f01cfb45c88d39d5345f86f55a0b8af622c4
docenkodan <docenkodan@gmail.com>
2018-04-05 23:44:48 +0300

    1

--- src/specks/MAIN.java
+++ src/specks/MAIN.java
@@ -1,8 +1,10 @@
 package specks;
 
+import java.io.IOException;
+
 public class MAIN {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Specks spk = new Specks();
 		spk.default_pos();
 		spk.print_tab();
@@ -17,5 +19,4 @@ public class MAIN {
 		
 		
 	}
-
 }
--- src/specks/Specks.java
+++ src/specks/Specks.java
@@ -1,5 +1,6 @@
 package specks;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class Specks {
@@ -42,15 +43,14 @@ public class Specks {
 				tab[i][j] = scan.nextInt();
 			}
 		}
-	}
-	
+	} 
 	
 	public int min_number_of_steps()	//Вычисление и вывод минимального количества ходов для решения
 	{
 		int i, j, step = 0, steps = 0;
 		for (i = 0; i<size; i++)
 		{
-			for (j = 0; j<size; j++)
+			for (j = 0; j<size; j++) 
 			{
 				if ((tab[i][j] != j+size*i+1)&&(tab[i][j] != 0))
 				{
@@ -71,5 +71,18 @@ public class Specks {
 		return steps;
 	}
 	
+	public void change(int i1, int j1, int i2,int j2)//Смена местами двух ячеек
+	{
+		int changer = 0;
+		changer = tab[i1][j1];
+		tab[i1][j1] = tab[i2][j2];
+		tab[i2][j2] = changer;
+	}
+	
+	public void decision ()
+	{
+		
+		
+	}
 	
 }
